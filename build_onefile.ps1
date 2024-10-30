4ed4f41684630a60e07947a18241231562b673f5
Nikita <n_ksh1v@outlook.com>
2024-10-31 01:49:34 +0300

    Create build_onefile.ps1

---
+++ build_onefile.ps1
@@ -0,0 +1,1 @@
+python -m nuitka --onefile displaceconverter.py --windows-icon-from-ico=".\assets\icon.ico" --windows-company-name="stakan" --windows-product-name="displace.bin converter" --windows-file-version=1.0.0 --windows-file-description="HTA displace.bin to raw/png GUI converter" --windows-console-mode=disable
